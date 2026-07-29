38904196af9248e8f29059012ad0995d087094d7
Larineon <sweetbun159@gmail.com>
2026-07-29 21:18:16 +1000

    Add files via upload

--- test_main_page.py
+++ test_main_page.py
@@ -1,58 +1,40 @@
-from page_objects.main_page import MainPage
 from page_objects.feed_page import FeedPage
+from page_objects.main_page import MainPage
 from conftest import *
 import allure
 
 
+@allure.feature('Главная страница')
 class TestMainPage:
-
-    @allure.title('Проверка перехода по клику на "Конструктор"')
-    @allure.description('Проверка навигации через кнопку "Конструктор" в хедере')
-    def test_navigate_to_constructor_success(self, driver):
-        main_page = MainPage(driver)
-        feed_page = FeedPage(driver)
-        
-        main_page.click_header_feed_button()        
-        main_page.click_on_button_constructor()
-        
-        assert 'Соберите бургер' in main_page.get_text_on_title_of_constructor()
-
-    @allure.title('Проверка перехода по клику на "Ленту заказов"')
-    @allure.description('Проверка перехода в раздел "Лента заказов"')
-    def test_navigate_to_order_history_success(self, driver):
-        main_page = MainPage(driver)
-        feed_page = FeedPage(driver)
-        
+    
+    @allure.story('Навигация')
+    @allure.title('Переход в ленту заказов по клику на кнопку')
+    def test_click_order_feed_button(self, main_page, feed_page):
+        "переход в ленту заказов"
         main_page.click_header_feed_button()
         
-        # Проверка: отображается заголовок "Лента заказов"
         assert feed_page.get_text_on_title_of_orders_list() == 'Лента заказов'
-
-    @allure.title('Проверка отображения окна "Детали ингредиента" при клике на ингредиент')
-    @allure.description('Проверка открытия модального окна с деталями ингредиента')
-    def test_displaying_modal_window_details_of_ingredient_success(self, driver):
-        main_page = MainPage(driver)        
-        main_page.click_on_ingredient()
+    
+    @allure.story('Навигация')
+    @allure.title('Переход в конструктор по клику на кнопку')
+    def test_click_constructor_button(self, main_page):
+        "переход в конструктор"
+        main_page.click_on_button_constructor()
         
-        # Проверка: модальное окно отображается
-        assert main_page.check_displaying_of_modal_details()
-
-    @allure.title('Проверка закрытия окна "Детали ингредиента" кликом по крестику')
-    @allure.description('Проверка закрытия модального окна с деталями ингредиента')
-    def test_close_modal_window_details_of_ingredient_success(self, driver):
-        main_page = MainPage(driver)
-        
-        main_page.click_on_ingredient()        
-        main_page.close_modal()
-        
-        # Проверка: модальное окно закрыто
-        assert main_page.check_not_displaying_of_modal_details()
-        
-    @allure.title('Проверка увеличения числа на счетчике при добавлении ингредиента в заказ')
-    @allure.description('Проверка работы счетчика ингредиентов при добавлении в заказ')
-    def test_changing_counter_for_ingredients_in_order_success(self, driver):
-        main_page = MainPage(driver)
-        
+        assert main_page.check_constructor_page_displayed()
+    
+    @allure.story('Корзина')
+    @allure.title('Корзина отображается на главной странице')
+    def test_basket_displayed(self, main_page):
+        "отображение корзины"
+        assert main_page.check_basket_displayed()
+    
+    @allure.story('Счетчик ингредиентов')
+    @allure.title('Счетчик ингредиента увеличивается при добавлении')
+    def test_ingredient_counter_increases(self, main_page):
+        "увеличение счетчика"
+        initial_count = main_page.get_ingredient_count()
         main_page.drag_and_drop_ingredient_to_order()
         
-        assert main_page.check_constructor_button_displayed()
+        new_count = main_page.get_ingredient_count()
+        assert new_count > initial_count
